029f5a4c6ac75793cfb5eb2dfa8279158e2db5bb
vicky acharjee <vickyacharjeevlg@gmail.com>
2024-01-27 23:23:29 +0530

    welcome commit

---
+++ welcome.java
@@ -0,0 +1,10 @@
+
+public class welcome{
+
+    public static void main(String[] args) {
+        System.out.println(1+1);
+
+            System.out.println("Story begins here.....:)");
+        
+    }
+}
