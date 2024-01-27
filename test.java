4e620c8e4c1d7c62dcdda9a1460754aa45b155fc
vicky acharjee <vickyacharjeevlg@gmail.com>
2024-01-27 22:57:00 +0530

    commit for save file

---
+++ test.java
@@ -0,0 +1,11 @@
+
+public class test{
+
+    public static void main(String[] args) {
+        System.out.println(1+1);
+
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i);
+        }
+    }
+}
