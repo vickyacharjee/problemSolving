1bcddf6530f5f792b26919b9f6559c82278784ab
vicky acharjee <vickyacharjeevlg@gmail.com>
2024-02-03 14:56:14 +0530

    commit for java

---
+++ Basics/Initials.java
@@ -0,0 +1,39 @@
+package Basics;
+
+public class Initials{
+    public static void main(String[] args) {
+        System.out.println("hello");
+
+        //  For loop demsontrartion
+
+        // for (int i = 0; i < 10; i++) {
+        //     System.out.println(i);
+        // }
+    
+        // for (int j = 1; j > 0; j++) {
+        //     System.out.println(j);
+
+        //     if (j==10000) {
+        //         break;
+        //     }
+        // }
+
+        //Ex with for loop::with range from 0 to 10
+
+        for (int i = 1; i <=10; i++) {
+            for (int j =0; j <=10; j++) {
+                System.out.println(i+"X"+j+"="+i*j);
+            }
+        }
+
+        // while LOOP demonstration
+
+        // while (true) {
+        //     System.out.println("hello from while ");
+        // }
+
+
+
+
+    }
+}
