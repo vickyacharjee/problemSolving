14171078bd9a45ce60acd745c5c547ab1c43e03c
vicky acharjee <vickyacharjeevlg@gmail.com>
2024-02-03 14:42:30 +0530

    commit for java

---
+++ Basics/test1.java
@@ -0,0 +1,35 @@
+package Basics;
+
+public class test1 {
+    public static void main(String[] args) {
+        System.out.println("hello");
+
+        //  For loop demsontrartion
+
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i);
+        }
+    
+        for (int j = 1; j > 0; j++) {
+            System.out.println(j);
+
+            if (j==10000) {
+                break;
+            }
+        }
+
+        // while LOOP demonstration
+
+        while (true) {
+            System.out.println("hello from while ");
+        }
+
+
+    }
+
+
+}
+
+
+
+
--- main/test1.java
+++
@@ -1,7 +0,0 @@
-package main;
-
-public class test1 {
-    public static void main(String[] args) {
-        System.out.println("hello");
-    }
-}
