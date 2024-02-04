86a39a5be6394e55d4a3d1fab2d18ddae091e71c
vicky acharjee <vickyacharjeevlg@gmail.com>
2024-02-04 18:16:01 +0530

    4-Feb

--- Basics/Initials.java
+++ Basics/Initials.java
@@ -12,26 +12,41 @@ public class Initials{
     
 
         //with break statement
-        for (int j = 1; j > 0; j++) {
-            System.out.println(j);
+        // for (int j = 1; j > 0; j++) {
+        //     System.out.println(j);
 
-            if (j==10000) {
-                break;
-            }
-        }
+        //     if (j==10000) {
+        //         break;
+        //     }
+        // }
 
         //Ex with for loop::with range from 0 to 10
 
-        for (int i = 1; i <=10; i++) {
-            for (int j =0; j <=10; j++) {
-                System.out.println(i+"X"+j+"="+i*j);
-            }
-        }
+        // for (int i = 1; i <=10; i++) {
+        //     for (int j =0; j <=10; j++) {
+        //         System.out.println(i+"X"+j+"="+i*j);
+        //     }
+        // }
 
         // while LOOP demonstration
 
-        while (true) {
-            System.out.println("hello from while ");
+        // while (true) {
+        //     System.out.println("hello from while ");
+        // }
+
+        //while EX
+
+        int a=20000;
+        while (a>1) {
+
+            System.out.println("done");
+            if (a==12) 
+                System.out.println("Applied break");
+                break;
+            
+            
+            
+
         }
 
 
