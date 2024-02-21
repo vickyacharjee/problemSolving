cb7532e3fd9cbcecbdaa3463502e5f83fdf92006
vicky acharjee <vickyacharjeevlg@gmail.com>
2024-02-21 22:25:32 +0530

    test for comment

--- Basics/linearSearch.java
+++ Basics/linearSearch.java
@@ -1,20 +1,19 @@
-// package Basics;
+package Basics;
 
-// public class linearSearch {
-//     public static void main(String[] args) {
-//       int[] arr={1,2,4};
-//       int search=10;
+public class linearSearch {
+    public static void main(String[] args) {
+      int[] arr={1,2,4};
+      int search=10;
 
-//       for (int i = 0; i < arr.length; i++) {
-//         if (arr[i]==search) {
-//             System.out.println("found at postion number: "+i);
-//         }
-//         else{
-//             System.out.println("not found");
-//         }
+      for (int i = 0; i < arr.length; i++) {
+        if (arr[i]==search) {
+            System.out.println("found at postion number: "+i);
+        }
+        else{
+            System.out.println("not found");
+        }
 
-//       }
-//       jsjsjsjjssj
-// hello this is vicky achatrjee and i am from class 110 b today i am stamndeing here to convey about my thought
-//     }
-// }
+      }
+     
+    }
+}
